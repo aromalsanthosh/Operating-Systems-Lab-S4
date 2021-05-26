96c0b2ba95ed9e83222fab39f78bd200b1a7aa95
Aromal S <aromalponmana@gmail.com>
2021-05-26 23:33:56 +0530

    Update 1_odd-even.c

--- Cycle-2/1_odd-even.c
+++ Cycle-2/1_odd-even.c
@@ -1,7 +1,5 @@
-/*C Program to create 2 process one parent process
- must bring odd numbers up to limit n. At the same
-time child process must bring even numbers up to n.
-Also print the process id*/
+/*C Program to create 2 process one parent process must bring odd numbers up to limit n.
+At the same time child process must bring even numbers up to n. Also print the process id*/
 #include <stdio.h>
 #include <unistd.h>
 
