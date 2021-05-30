401ac5c94ecc17eb189b19e7f345e8d6582982e7
aromalsanthosh <aromalponmana@gmail.com>
2021-05-30 22:31:08 +0530

    Added-Q2

--- Cycle-2/2_single-thread.c
+++ Cycle-2/2_single-thread.c
@@ -4,3 +4,36 @@
 #include <unistd.h>
 #include <pthread.h>
 
+int global[2];
+
+void *sumOf(void *arg)
+{
+    int *arr;
+    arr = arg;
+
+    int n1,n2,sum;
+    n1=arr[0];
+    n2=arr[1];
+    sum = n1+n2;
+
+    printf("Sum , %d + %d = %d\n",n1,n2,sum);
+
+    return NULL;
+}
+
+int main() 
+{
+    printf("Enter First number : ");
+    scanf("%d",&global[0]);
+
+    printf("Enter Second number : ");
+    scanf("%d",&global[1]);
+
+    pthread_t add; //unsigned integer value that stores the thread id of the thread created.
+    pthread_create(&add,NULL,sumOf,global); //thread creation
+    pthread_join(add,NULL);//waiting for the termination of a thread.
+    printf("Thread ID: %lu ",add );//Printing thread ID
+
+    return 0;
+}
+// Reference : https://www.geeksforgeeks.org/thread-functions-in-c-c/
