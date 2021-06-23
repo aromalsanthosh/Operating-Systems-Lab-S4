f10be46d4fdc672aeed6f616f0976ce010dfc88f
Aromal S <aromalponmana@gmail.com>
2021-06-23 12:41:23 +0530

    Create 1_FCFS.c

---
+++ Cycle-3/CPU Scheduling/1_FCFS.c
@@ -0,0 +1,3 @@
+/*First in, first out (FIFO), also known as first come, first served (FCFS), is the simplest scheduling algorithm.
+ FIFO simply queues processes in the order that they arrive in the ready queue.
+ In this, the process that comes first will be executed first and next process starts only after the previous gets fully executed.*/
