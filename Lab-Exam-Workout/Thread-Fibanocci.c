ea80cfeab4f1cc0d6a5b1084389565064b973a4a
aromalsanthosh <aromalponmana@gmail.com>
2021-10-02 11:24:01 +0530

    Update Thread Fibanocci

--- Lab-Exam-Workout/Thread-Fibanocci.c
+++ Lab-Exam-Workout/Thread-Fibanocci.c
@@ -40,3 +40,37 @@ int main()
 
     return 0;
 }
+
+/**
+ * C program to print Fibonacci series up to n terms
+ */
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a, b, c, i, terms;
+
+//     /* Input number from user */
+//     printf("Enter number of terms: ");
+//     scanf("%d", &terms);
+
+//     /* Fibonacci magic initialization */
+//     a = 0;
+//     b = 1;
+//     c = 0;
+
+//     printf("Fibonacci terms: \n");
+
+//     /* Iterate through n terms */
+//     for(i=1; i<=terms; i++)
+//     {
+//         printf("%d, ", c);
+
+//         a = b;     // Copy n-1 to n-2
+//         b = c;     // Copy current to n-1
+//         c = a + b; // New term
+//     }
+
+//     return 0;
+// }
